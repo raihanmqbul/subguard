95dc9c6709999c831369d3a75c7cc2a12192f97e
Raihan <rehan.maqbul@gmail.com>
2026-03-30 15:47:35 +0500

    Restructure repository to contain only landing page

--- src/sidepanel/context/OnlineContext.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const OnlineContext = createContext<boolean>(true);
-
-export function useOnlineContext(): boolean {
-  return useContext(OnlineContext);
-}
